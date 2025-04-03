b691656f65b06594c47732cbacb74dd711b8156c
Netishyn <sergeyladnyak7@gmail.com>
2025-04-03 21:28:20 +0300

    fix register

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -12,7 +12,7 @@ import { yupResolver } from "@hookform/resolvers/yup";
 const Register = ({ onClose }) => {
 
     const schema = yup.object().shape({
-        name: yup.string().min(3).max(20).required(),
+        displayName: yup.string().min(3).max(20).required(),
         email: yup.string().email().required(),
         password: yup.string().min(8).max(32).required(),
     });
